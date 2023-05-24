7eef785cd3c8f07c68b037d92bc702b020f0c63d
VaishnaviP666 <122201508+VaishnaviP666@users.noreply.github.com>
2023-05-24 16:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+var els = document.getElementsByTagName("strong");
+  for (var i = 0; i < els.length; i++) {
+    els[i].style.color = "green";
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+var els = document.getElementsByTagName("strong");
+  for (var i = 0; i < els.length; i++) {
+    els[i].style.color = "black";
+  }
     
 }
